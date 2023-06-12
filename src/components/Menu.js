743da47d1e83295ca9d254163be4d1bc8eb25104
Aluoch Laurine <laurinealuoch7@gmail.com>
2023-06-12 16:49:05 +0300

    update logoitem size

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,7 +4,7 @@ import logo from '../images/logo.svg';
 const LogoItem = ({logoItem}) => {
     return (
         <div class="flex items-center flex-shrink-0 pl-4">
-            <img class="h-8 w-10 hover:object-scale-down" 
+            <img class="h-20 w-100 hover:object-scale-down" 
             src ={logoItem}
             alt='Aluoch Laurine Logo'/>
         </div>
@@ -15,7 +15,7 @@ const MenuItem = ({section, href}) => {
     return (
         <a 
         href={href}
-        class="text-lg text-green py-4 px-2 border-green hover:text-pink font-thin"
+        class="text-lg text-green py-4 px-2 border-green hover:text-pink font-regular"
         > 
             {section}
         </a>
