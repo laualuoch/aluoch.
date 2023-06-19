dadf295157a0a51699d607da22b90003518002e4
Aluoch Laurine <laurinealuoch7@gmail.com>
2023-06-19 12:40:19 +0300

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ const Footer = ({currentYear}) => {
     return (
         <footer class="dark:bg-gray-900 m-4">
             <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-                <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© {currentYear} <a href="" class="hover:underline"></a>. Designed and Developed by Aluoch Laurine.</span>
+                <span class="text-lg text-green py-4 px-2 border-green hover:text-pink font-regular font-chilanka block sm:text-center dark:text-gray-400">© {currentYear}. Designed and Developed by Aluoch Laurine.</span>
             </div>
         </footer>
     )
