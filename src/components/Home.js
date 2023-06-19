fd481ead0e96235cb49e0088746d3bfd2e2cc4f4
Aluoch Laurine <laurinealuoch7@gmail.com>
2023-06-19 13:32:38 +0300

    use flex instead of grids

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 
 const Home = () => {
     return (
-        <div class="grid grid-cols-2 gap-2">
+        <div class="flex flex-row justify-between items-center gap-2 p-20">
             <div>
                 <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt="" />
             </div>
