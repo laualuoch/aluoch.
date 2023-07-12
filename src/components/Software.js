0d749cc69e5a89b7a10660c2c6541b574eadc789
Aluoch Laurine <laurinealuoch7@gmail.com>
2023-07-12 19:21:55 +0300

    update software portfolio page

--- src/components/Software.js
+++ src/components/Software.js
@@ -1,7 +1,23 @@
 import '../assets/main.css'
 import logo from '../images/logo.svg';
 
-const Software = () => {
+const Hamburger = () => {
+    return (
+        <nav class="flex pl-20 pr-20 text-gray-700 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700" aria-label="Breadcrumb">
+            <ol class="inline-flex items-center space-x-1 md:space-x-3">
+                <a href="#" class="inline-flex items-center text-lg text-green py-4 px-2 border-green hover:text-pink font-regular font-chilanka sm:text-center dark:text-gray-400">
+                    HOME
+                </a>
+                <a href="#" class="inline-flex items-center text-lg text-green py-4 px-2 border-green hover:text-pink font-regular font-chilanka sm:text-center dark:text-gray-400">
+                    SOFTWARE ENGINEERING 
+                </a>
+            </ol>
+        </nav>
+    )
+};
+
+
+const SoftwareItems = () => {
     return (
         <div
         id="carouselExampleControls"
@@ -108,6 +124,16 @@ const Software = () => {
         </button>
     </div>  
     )
+};
+
+const Software = () => {
+    return (
+        <>
+        <Hamburger />
+        <SoftwareItems />
+        </>
+    )
+
 };
 
 export default Software;
