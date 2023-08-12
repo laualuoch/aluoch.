3606890d98c028e4e16d3cfc44ec0a197fa47bce
Aluoch Laurine <laurinealuoch7@gmail.com>
2023-08-12 12:04:06 +0300

    update homepage

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -3,36 +3,31 @@ import  image from "./../../images/aluoch.jpg"
 
 const HomePage = () => {
     return (
-        <div className="container row justify-between center">
-            <div className="col-4">
-                <img 
-                    className="img-fluid rounded" 
-                    src={image} 
-                    alt="user image" 
-                />
-                <div>
-                    socials
+        <div className="container justify-items-center justify-center align-middle">
+            <section class="ftco-section ftco-intro">
+                <div className="container">
+                    <div class="row align-items-center"> 
+                        <div className="col-md-4 text-center">
+                            <img 
+                                className="img-fluid rounded-circle" 
+                                src={image} 
+                                alt="user image" 
+                            />
+                        </div>
+                        <div className="col-md-8">
+                            <h2>
+                                Hi, I am Aluoch Laurine
+                            </h2>
+                            <h4>I am a Software Engineer, 
+                                a Product Designer and a Technical writer. </h4>
+                            <button className="btn btn-primary mt-auto">
+                                View Portfolio
+                            </button>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div className="col-8">
-                <h1>
-                    Hi, I am Aluoch Laurine
-                </h1>
-                <p>
-                    Welcome to the digital embodiment of my
-                    journey as a versatile software engineer, adept UI/UX designer, 
-                    and a skilled technical writer. I invite you to explore 
-                    the chapters of my professional narrative. 
-                    This virtual tome presents my expertise, 
-                    education, work experiences, and my accomplished projects 
-                    in the same regard.
-                </p>
-                <button>
-                    Skills
-                </button>
-            </div>
+            </section>
         </div>
-        
     );
 }
 
