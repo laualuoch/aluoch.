e6fe0c40485e1f90c5f81ab55192ea2e6a9be688
Aluoch Laurine <laurinealuoch7@gmail.com>
2023-08-13 00:55:12 +0300

    update homepage

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -6,13 +6,12 @@ const HomePage = () => {
                 <img
                     className="mb-4"
                     src=""
-                    width="100"
-                    height="100" 
                     alt="intro-image" />
-                <h4 className="text-3xl text-white font-chilanka font-bold mb-4">
-                    Hi, I'm <span className="text-crayola border-b">Aluoch</span>
+                <h4 className="text-5xl text-beige font-chilanka font-bold mb-4">
+                    Hi, I'm <span className="text-white">Aluoch.</span>
                 </h4>
-                <h2 className="text-2xl text-white font-chilanka font font-semibold mb-4">Welcome to the digital embodiment of my journey as a versatile 
+                <h2 className="text-2xl text-beige font-chilanka font font-semibold mb-4">
+                    Welcome to the digital embodiment of my journey as a versatile 
                     software engineer, product designer and a skilled technical
                      writer. This online catalogue provides the different chapters of
                       my professional narrative. </h2>
