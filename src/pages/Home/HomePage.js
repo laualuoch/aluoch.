992a85a333a8376d107448694e059a9f34e2094d
Aluoch Laurine <laurinealuoch7@gmail.com>
2023-08-13 00:46:26 +0300

    add margin bottom

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -4,14 +4,15 @@ const HomePage = () => {
         <div className="">
             <div className="d-flex-start ">
                 <img
+                    className="mb-4"
                     src=""
                     width="100"
                     height="100" 
                     alt="intro-image" />
-                <h4 className="text-xl text-white font-chilanka font-bold">
+                <h4 className="text-xl text-white font-chilanka font-bold mb-4">
                     Hi, I'm <span className="text-crayola border-b">Aluoch</span>
                 </h4>
-                <h2 className="text-lg text-white font-chilanka font font-semibold">Welcome to the digital embodiment of my journey as a versatile 
+                <h2 className="text-lg text-white font-chilanka font font-semibold mb-4">Welcome to the digital embodiment of my journey as a versatile 
                     software engineer, product designer and a skilled technical
                      writer. This online catalogue provides the different chapters of
                       my professional narrative. </h2>
