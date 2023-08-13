d958fe942c368caa887198805f04fd609f9c0669
Aluoch Laurine <laurinealuoch7@gmail.com>
2023-08-13 03:46:05 +0300

    WIP:refactor Cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,9 +3,9 @@ const Card = ({title, description}) => {
     return (
         <div class="max-w-sm rounded overflow-hidden shadow-lg">
             <div class="px-6 py-4">
-                <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+                <div class="font-bold text-xl mb-2">{ title }</div>
                 <p class="text-gray-700 text-base">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                    { description }
                 </p>
             </div>
             <div class="px-6 pt-4 pb-2">
@@ -17,3 +17,5 @@ const Card = ({title, description}) => {
 
     );
 }
+
+export default Card;
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,19 +1,10 @@
+import Card from "../../components/Card";
 
-const About = ({title, description}) => {
+const About = () => {
     return (
-        <div class="max-w-sm rounded overflow-hidden shadow-lg">
-            <div class="px-6 py-4">
-                <div class="font-bold text-xl mb-2">{ title }</div>
-                <p class="text-gray-700 text-base">
-                { description }
-                </p>
-            </div>
-            <div class="px-6 pt-4 pb-2">
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-                </div>
-        </div>
+       <Card 
+        title="Product Design" 
+        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled i"/>
     );
 }
 
