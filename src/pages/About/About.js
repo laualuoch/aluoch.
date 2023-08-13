96da160f0c9b1ce84ee987996dde86c1589cc7b5
Aluoch Laurine <laurinealuoch7@gmail.com>
2023-08-13 04:11:42 +0300

    WIP: refactor Cards

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,11 +1,24 @@
+import React from "react"; 
+
 import Card from "../../components/Card";
 
 const About = () => {
+    const aboutItems = [
+        {title: "Software Engineering", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled i", tools: ["HTML & CSS", "JavaScript", "React.js", "Python", "Kotlin", "Jetpack Compose"]},
+        {title: "Product Design", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled i", tools: ["Figma"]},
+        {title: "Technical writing", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled i", tools: []}
+    ];
+
     return (
-       <Card 
-        title="Product Design" 
-        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled i"/>
+        aboutItems.map(({title, description, tools}) => (
+            <Card
+                key={title} 
+                title={title}
+                description={description}
+                tools={tools}
+            />
+        ))
     );
-}
+};
 
 export default About;
