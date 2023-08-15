c2c1e6b6d1e04130d614046e7143f43a6af0d55b
Aluoch Laurine <laurinealuoch7@gmail.com>
2023-08-15 10:09:40 +0300

    Update about page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,7 +1,27 @@
 import React from "react";
 import Card from "../../components/Card";
+import Breadcrumb from "../../components/Breadcrumb";
+import { useNavigate } from "react-router-dom";
 
 const About = () => {
+    const navigate = useNavigate();
+
+    const navigateToHome = () => {
+        navigate('/');
+    }
+
+    const navigateToAbout = () => {
+        navigate('/about');
+    }
+    const handleButtonClick = () => {
+        navigate('/about')
+    }
+
+    const navItems = [
+        {link:"Home", linkOnClick: navigateToHome},
+        {link:"About", linkOnClick: navigateToAbout}
+    ];
+
     const aboutItems = [
         {title: "Software Engineering", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled i", tools: ["HTML & CSS", "JavaScript", "React.js", "Python", "Kotlin", "Jetpack Compose"]},
         {title: "Product Design", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled i", tools: ["Figma"]},
@@ -9,7 +29,9 @@ const About = () => {
     ];
 
     return (
-        <div className="flex flex-wrap justify-center">
+        <>
+         <Breadcrumb navItems={navItems}/>
+         <div className="flex flex-wrap justify-center">
             {aboutItems.map(({ title, description, tools }) => (
                 <Card
                     key={title}
@@ -20,6 +42,8 @@ const About = () => {
                 />
             ))}
         </div>
+        </>
+       
 
     );
 };
