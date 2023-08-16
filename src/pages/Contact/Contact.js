8971ae03b9188f314397bd4bf02fa4a39c9ff967
Aluoch Laurine <laurinealuoch7@gmail.com>
2023-08-17 00:24:46 +0300

    Update contact page

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -44,7 +44,7 @@ const Contact = () => {
                     <h2 class="mb-4 text-3xl lg:text-4xl tracking-tight font-chilanka font-extrabold text-beige">Contact.</h2>
                     <p class="font-light font-chilanka text-white sm:text-xl dark:text-gray-400">Let's Code the Future Together: Reach Out and Connect!</p>
                 </div> 
-                <div class="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
+                <div class="mx-auto max-w-screen-sm text-center lg:mb-8 mb-4">
                     <Socials />
                 </div> 
                 <div className="flex flex-wrap justify-center w-full">
