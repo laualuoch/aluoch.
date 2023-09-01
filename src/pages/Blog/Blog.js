af5644507a06fd8ea577a8eb59aec96b5961d8a1
Aluoch Laurine <laurinealuoch7@gmail.com>
2023-09-01 14:10:52 +0300

    REvert changes towards backend development. Code still contains commented out code for backend integration

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -4,18 +4,23 @@ import BlogCard from "../../components/BlogCard";
 import Loading from "../../components/Loading";
 
 const Blog = ({sectionId}) => {
-    const [blogData, setBlogData] = useState([{}]);
 
-    useEffect(()=> {
-        fetch('/blog').then(
-            res => res.json()
-        ).then(
-            blogData => {
-                setBlogData(blogData)
-                console.log(blogData)
-            }
-        )
-    }, [])
+    const blogData = [
+        { category: "Python", title: "Mastering Python Errors, Exceptions, and Exception Handling: A Comprehensive Guide - Part One", intro: "In the Tiobe Index Report as of 2023, Python is the most widely used programming language. This is considerably so given the simplicity and versatility of the language. Now, as a developer, it is important to build robust applications that are not only functional but also efficient.", link:"https://medium.com/@aluoch-laurine/part-one-python-errors-exceptions-and-exception-handling-a-comprehensive-guide-aa9b75772adf"},
+        { category: "Python", title: "Mastering Python Errors, Exceptions, and Exception Handling: A Comprehensive Guide - Part Two", intro: "In the previous article, we mentioned that in Python programming, there are two distinguishable Errors; syntax errors and exceptions. In this article, we are going to explore exceptions, taking a dive into the different exceptions and how to handle them.", link:"https://medium.com/@aluoch-laurine/part-one-python-errors-exceptions-and-exception-handling-a-comprehensive-guide-aa9b75772adf" }
+    ]
+    // const [blogData, setBlogData] = useState([{}]);
+
+    // useEffect(()=> {
+    //     fetch('/').then(
+    //         res => res.json()
+    //     ).then(
+    //         blogData => {
+    //             setBlogData(blogData)
+    //             console.log(blogData)
+    //         }
+    //     )
+    // }, [])
 
     return (
         <section id={sectionId} class="flex items-center justify-center h-screen">
@@ -25,18 +30,20 @@ const Blog = ({sectionId}) => {
                     <p class="font-light font-chilanka text-white sm:text-xl dark:text-gray-400">Exploring the Tech World One Line at a Time.</p>
                 </div> 
                 <div class="grid gap-8 lg:grid-cols-2">
-                    {(typeof blogData.articles === 'undefined') ? (
+                    {/* {(typeof blogData.articles === 'undefined') ? (
                         <Loading />
-                    ) : (
-                        blogData.articles?.map(({key, category, title, intro, link}) => (
+                    ) : ( */}
+                    {
+                        blogData.map(({key, category, title, intro, link}) => (
                             <BlogCard
-                            key={key}
-                            category={category}
-                            title={title}
-                            intro={intro}
-                            link={link} />   
+                                key={key}
+                                category={category}
+                                title={title}
+                                intro={intro}
+                                link={link} 
+                            />   
                         ))     
-                    )}                    
+                    }                    
                 </div>  
             </div>
         </section>
