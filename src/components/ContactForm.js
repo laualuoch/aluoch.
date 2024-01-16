0b5750e73b837a51ed712ad4532947c6eac42915
Aluoch Laurine <laurinealuoch7@gmail.com>
2024-01-16 08:03:05 +0300

    Extract Text Components

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Button from "./Buttons";
 import { Input, MultilineInput }from "./Inputs";
-import ErrorText from "./ErrorText";
+import { ErrorText } from "./Text";
 import emailjs from '@emailjs/browser';
 
 class ContactForm  extends React.Component {
--- src/components/Text.js
+++ src/components/Text.js
@@ -1,7 +1,7 @@
-const ErrorText = ({text}) => {
+
+
+export const ErrorText = ({text}) => {
     return (
         <h1 className="text-red font-md font-chilanka">{text}</h1>
     )
 }
-
-export default ErrorText;
