a437e96f45eee33396db8d9bd1f8154447bab88c
Aluoch Laurine <laurinealuoch7@gmail.com>
2024-01-23 00:21:27 +0300

    Clean up index.js

--- src/index.js
+++ src/index.js
@@ -1,37 +1,38 @@
-import * as React from 'react';
-import ReactDOM from "react-dom/client";
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import reportWebVitals from './reportWebVitals';
-import Layout from './pages/Layout';
-import './assets/tailwind.css';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Portfolio from './pages/Portfolio';
-import Blog from './pages/Blog';
-
+import * as React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+import Layout from "./pages/Layout";
+import "./assets/tailwind.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+import Blog from "./pages/Blog";
 
 export default function App() {
   return (
     <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={ <Layout /> }>
-          <Route index element={ <Home sectionId='home' />} />
-          <Route path="about" element={ <About sectionId='about' />  } />
-          <Route path="portfolio" element={ <Portfolio sectionId='portfolio' />} />
-          <Route path="blog" element={ <Blog sectionId='blog' /> } />
-          <Route path="contact" element={ <Contact sectionId='contact' />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>
-  )
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home sectionId="home" />} />
+            <Route path="about" element={<About sectionId="about" />} />
+            <Route
+              path="portfolio"
+              element={<Portfolio sectionId="portfolio" />}
+            />
+            <Route path="blog" element={<Blog sectionId="blog" />} />
+            <Route path="contact" element={<Contact sectionId="contact" />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>
+  );
 }
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 root.render(<App />);
 
 reportWebVitals();
