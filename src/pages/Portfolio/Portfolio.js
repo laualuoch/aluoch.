0dfa587e2b5ab9ccecd5a4b1b3f074f27e58320d
Aluoch Laurine <laurinealuoch7@gmail.com>
2024-01-22 20:55:30 +0300

    More changes

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -17,7 +17,7 @@ const Portfolio = ({sectionId}) => {
         <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
             <SectionHeader
             title="PORTFOLIO"
-            description="Elevating Ideas Through Design and Innovation: Explore My Portfoli"
+            description="Elevating Ideas Through Design and Innovation: Explore My Portfolio"
             />
             <div className="flex flex-wrap justify-center w-full">
                 {portfolioData.map(({ title, description, tools }) => (
