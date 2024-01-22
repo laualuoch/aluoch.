657963fdb50e9fba55866ceb6a45ff7ef6b30557
Aluoch Laurine <laurinealuoch7@gmail.com>
2024-01-22 20:49:51 +0300

    multiple chanegs in Homepage content

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,7 +2,7 @@
 
 export function InfoCard({text}) {
     return (
-        <span class="bg-green text-white font-chilanka border border-white inline-block rounded-full px-3 py-1 text-sm font-regular  mr-2 mb-2">{text}</span>
+        <span class="bg-green text-white font-lato border border-white inline-block rounded-full px-3 py-1 text-sm  mr-2 mb-2">{text}</span>
     );
 }
 
--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -18,28 +18,20 @@ const HomePage = ({sectionId}) => {
             <div className="flex flex-col items-center justify-center">
                 <div className="container mx-auto">
                     <div className="flex flex-col lg:flex-row  md:flex-col sm:flex-col items-center justify-center p-6 space-x-8">
-                        <div className="flex items-center justify-center">
-                            {/* <img
-                                className="object-contain h-1/4 w-1/4 mb-4 rounded-full"
-                                src={aluoch}
-                                alt="intro-image"
-                            /> */}
-                        </div>
-                        <div className="flex flex-col justify-center">
-                            <h4 className="text-3xl lg:text-5xl text-beige font-chilanka font-bold mb-2 lg:mb-4">
+                        <div className="flex flex-col justify-center w-3/4">
+                            <h4 className="text-3xl lg:text-5xl text-beige font-lato font-bold mb-2 lg:mb-4">
                                 Hi, I'm <span className="text-white">Aluoch.</span>
                             </h4>
-                            <h2 className="text-lg lg:text-xl text-beige font-chilanka font-semibold mb-2 lg:mb-4">
-                                Welcome to the digital embodiment of my journey as a versatile
-                                software engineer, product designer and a skilled technical
-                                writer. I am <span className="text-white font-bold">currently looking for a new role </span>
-                                that will challenge me. I am also a part time student at
+                            <h2 className="text-lg lg:text-xl text-beige font-lato mb-2 lg:mb-4">
+                                This is a digital folder of my journey as a software engineer, product designer 
+                                and a skilled technical writer. I am currently 
+                                working part time at <span className="text-white font-semibold">Mimi Life.</span> I am also a part time student at
                                 <span className="text-white font-bold"> The University of East London </span>
-                                pursuing my <span className="text-white font-bold">Master's</span> in 
-                                <span className="text-white font-bold"> Computer Science</span>
+                                pursuing my <span className="text-white font-bold">Master's Degree</span> in 
+                                <span className="text-white font-bold"> Computer Science.</span>
                             </h2>
                             <div className="flex flex-wrap mt-4 justify-center w-full">
-                                <h4 className="text-xl lg:text-3xl text-white font-chilanka font-bold mb-2 lg:mb-4">
+                                <h4 className="text-xl lg:text-3xl text-white font-lato font-bold mb-2 lg:mb-4">
                                     Tech Stack
                                 </h4>
                                 <div className="flex flex-wrap mt-4 justify-center w-full">
