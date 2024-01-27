bd5e1c36511cdf3416eb6140f24ba30c67844155
Aluoch Laurine <laurinealuoch7@gmail.com>
2024-01-28 00:25:11 +0300

    Update Home page content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { InfoCard } from "../components/Cards";
+import aluoch from './../images/aluoch.jpg'
 
 const Home = ({sectionId}) => {
     const techStack = [
@@ -12,28 +13,36 @@ const Home = ({sectionId}) => {
         {text:"Jetpack Compose", projects: "visit project"}
     ];
 
+
     return (
         <section id={sectionId} class="flex items-center justify-center p-6">
             <div className="flex flex-col items-center justify-center">
                 <div className="container mx-auto">
                     <div className="flex flex-col lg:flex-row  md:flex-col sm:flex-col items-center justify-center p-6 space-x-8">
+                        <div className="flex items-center justify-center">
+                            <img
+                                className="object-contain h-1/4 w-1/4 mb-4 rounded-full"
+                                src={aluoch}
+                                alt=""
+                            />
+                        </div>
                         <div className="flex flex-col justify-center items-center w-3/4">
-                            <h4 className="text-3xl lg:text-5xl text-beige font-lato font-bold mb-2 lg:mb-4">
-                                Hi, I'm <span className="text-white">Aluoch.</span>
-                            </h4>
-                            <h2 className="text-lg lg:text-xl text-beige font-lato mb-2 lg:mb-4">
-                                This is a digital folder of my journey as a software engineer, product designer 
-                                and a skilled technical writer. I am currently 
-                                working part time at <span className="text-white font-semibold">Mimi Life.</span> I am also a part time student at
-                                <span className="text-white font-bold"> The University of East London </span>
-                                pursuing my <span className="text-white font-bold">Master's Degree</span> in 
-                                <span className="text-white font-bold"> Computer Science.</span>
-                            </h2>
-                            <div className="flex flex-wrap mt-4 justify-center w-full">
-                                <h4 className="text-xl lg:text-3xl text-white font-lato font-bold mb-2 lg:mb-4">
-                                    Tech Stack
-                                </h4>
-                                <div className="flex flex-wrap mt-4 justify-center w-full">
+                            <p className="text-lg lg:text-xl text-beige font-lato mb-2 lg:mb-4">
+                                I am a user-centric sofwtare engineer who enjoys crafting solutions 
+                                with the end user in mind. I am a naturally observant and empathtic 
+                                individual, making the process quite straightforward. 
+                            </p>
+                            <p className="text-lg lg:text-xl text-beige font-lato mb-2 lg:mb-4">
+                                I am currently a part time student at
+                                <span className="text-white text-lg lg:text-xl"> The University of East London </span>
+                                pursuing my <span className="text-white text-lg lg:text-xl">Master's Degree</span> in 
+                                <span className="text-white text-lg lg:text-xl"> Computer Science.</span>
+                            </p>
+                            <div className="flex flex-wrap mt-4 justify-start w-full">
+                            <p className="text-lg lg:text-xl text-beige font-lato mb-2 lg:mb-4">
+                                I have worked with nultiple technologies including:
+                            </p>
+                                <div className="flex flex-wrap mt-4 justify-start w-full">
                                     {techStack?.map(({text})=> (
                                         <InfoCard text={text} />
                                     ))}
