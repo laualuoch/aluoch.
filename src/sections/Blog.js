076c44c801325372e67a392cd18df3a1b6fa5fd6
Aluoch Laurine <laurinealuoch7@gmail.com>
2024-03-18 15:41:28 +0300

    Update heading blog heading

--- src/sections/Blog.js
+++ src/sections/Blog.js
@@ -6,7 +6,7 @@ const Blog = ({sectionId}) => {
 
     const blogData = [
         { category: "Python", title: "Mastering Python Errors, Exceptions, and Exception Handling: A Comprehensive Guide - Part One", intro: "In the Tiobe Index Report as of 2023, Python is the most widely used programming language. This is considerably so given the simplicity and versatility of the language. Now, as a developer, it is important to build robust applications that are not only functional but also efficient.", link:"https://medium.com/@aluoch/part-one-python-errors-exceptions-and-exception-handling-a-comprehensive-guide-aa9b75772adf"},
-        { category: "JavaScript", title: "Demystifying asynchronous programming in JavaScript", intro: "JavaScript is synchronous by nature, which means operations are executed in the chronological order that they are called. The implication of the synchronous nature of JavaScript is the delay in execution as long-running tasks are run to completion before proceeding ones are executed.", link:"https://medium.com/@aluoch/demystifying-asynchronous-programming-in-javascript-80e18145d117" }
+        { category: "JavaScript", title: "Demystifying Asynchronous Programming in JavaScript", intro: "JavaScript is synchronous by nature, which means operations are executed in the chronological order that they are called. The implication of the synchronous nature of JavaScript is the delay in execution as long-running tasks are run to completion before proceeding ones are executed.", link:"https://medium.com/@aluoch/demystifying-asynchronous-programming-in-javascript-80e18145d117" }
     ]
 
     return (
